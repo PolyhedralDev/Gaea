d605ac394161930fcb2b89a3776f6bf2651bdd59
dfsek <dfsek@protonmail.com>
2020-09-20 03:31:36 -0700

    Allow arbitrarily sized BiomeGrid

--- src/main/java/org/polydev/gaea/biome/BiomeGrid.java
+++ src/main/java/org/polydev/gaea/biome/BiomeGrid.java
@@ -8,10 +8,14 @@ public abstract class BiomeGrid {
     private final FastNoise biome;
     private final FastNoise climate;
     private final World world;
+    private final int sizeX;
+    private final int sizeZ;
     private Biome[][] grid;
     private NormalType normal = NormalType.LOOKUP;
 
     public BiomeGrid(World w, float freq1, float freq2) {
+        this.sizeX = 16;
+        this.sizeZ = 16;
         this.world = w;
         this.biome = new FastNoise((int) w.getSeed());
         this.biome.setNoiseType(normal.getNoiseType());
@@ -19,16 +23,42 @@ public abstract class BiomeGrid {
         this.climate = new FastNoise((int) w.getSeed() + 1);
         this.climate.setNoiseType(normal.getNoiseType());
         this.climate.setFrequency(freq2);
+        if(normal.getNoiseType().equals(FastNoise.NoiseType.Cellular)) {
+            FastNoise base = new FastNoise((int) w.getSeed());
+            base.setNoiseType(FastNoise.NoiseType.Simplex);
+            base.setFrequency(freq1);
+            biome.setCellularNoiseLookup(base);
+            biome.setCellularDistanceFunction(FastNoise.CellularDistanceFunction.Natural);
+            FastNoise base2 = new FastNoise((int) w.getSeed()+1);
+            base2.setNoiseType(FastNoise.NoiseType.Simplex);
+            base2.setFrequency(freq2);
+            climate.setCellularNoiseLookup(base2);
+            climate.setCellularDistanceFunction(FastNoise.CellularDistanceFunction.Natural);
+        }
     }
 
-    public BiomeGrid(int seed) {
-        this.world = null;
-        this.biome = new FastNoise(seed);
+    public BiomeGrid(World w, float freq1, float freq2, int sizeX, int sizeZ) {
+        this.sizeX = sizeX;
+        this.sizeZ = sizeZ;
+        this.world = w;
+        this.biome = new FastNoise((int) w.getSeed());
         this.biome.setNoiseType(normal.getNoiseType());
-        this.biome.setFrequency((float) 1 / 256);
-        this.climate = new FastNoise(seed + 1);
-        this.climate.setNoiseType(FastNoise.NoiseType.Value);
-        this.climate.setFrequency((float) 1 / 128);
+        this.biome.setFrequency(freq1);
+        this.climate = new FastNoise((int) w.getSeed() + 1);
+        this.climate.setNoiseType(normal.getNoiseType());
+        this.climate.setFrequency(freq2);
+        if(normal.getNoiseType().equals(FastNoise.NoiseType.Cellular)) {
+            FastNoise base = new FastNoise((int) w.getSeed());
+            base.setNoiseType(FastNoise.NoiseType.Simplex);
+            base.setFrequency(freq1);
+            biome.setCellularNoiseLookup(base);
+            biome.setCellularDistanceFunction(FastNoise.CellularDistanceFunction.Natural);
+            FastNoise base2 = new FastNoise((int) w.getSeed()+1);
+            base2.setNoiseType(FastNoise.NoiseType.Simplex);
+            base2.setFrequency(freq2);
+            climate.setCellularNoiseLookup(base2);
+            climate.setCellularDistanceFunction(FastNoise.CellularDistanceFunction.Natural);
+        }
     }
 
     public void setNormalType(NormalType n) {
@@ -55,7 +85,7 @@ public abstract class BiomeGrid {
     public Biome getBiome(int x, int z) {
         float biomeNoise = biome.getNoise((float) x, (float) z);
         float climateNoise = climate.getNoise((float) x, (float) z);
-        return grid[normal.normalize(biomeNoise)][normal.normalize(climateNoise)];
+        return grid[normal.normalize(biomeNoise, sizeX)][normal.normalize(climateNoise, sizeZ)];
     }
 
     /**
@@ -67,17 +97,19 @@ public abstract class BiomeGrid {
     public Biome getBiome(Location l) {
         float biomeNoise = biome.getNoise((float) l.getBlockX(), (float) l.getBlockZ());
         float climateNoise = climate.getNoise((float) l.getBlockX(), (float) l.getBlockZ());
-        return grid[normal.normalize(biomeNoise)][normal.normalize(climateNoise)];
+        return grid[normal.normalize(biomeNoise, sizeX)][normal.normalize(climateNoise, sizeZ)];
     }
 
     public World getWorld() {
         return world;
     }
 
+
+
     public enum NormalType {
         LEGACY {
             @Override
-            protected int normalize(double i) {
+            protected int normalize(double i, int range) {
                 if(i > 0) i = Math.pow(i, 0.8125); // Redistribute
                 else i = - Math.pow(- i, 0.8125); // Redistribute
                 return Math.min((int) Math.floor((i + 1) * 8), 15);
@@ -96,7 +128,7 @@ public abstract class BiomeGrid {
             private final double[] lookup = new double[] {- 0.30535656213760376D, - 0.24068142473697662D, - 0.1912580281496048D, - 0.1484183818101883D, - 0.10884492099285126D, - 0.07152662426233292D, - 0.03578951954841614D, - 7.427185773849487E-4D, 0.03426129370927811D, 0.07007939368486404D, 0.10747101902961731D, 0.147150918841362D, 0.19017954170703888D, 0.23992890119552612D, 0.3045589327812195D, 0.6485831141471863D};
 
             @Override
-            protected int normalize(double i) {
+            protected int normalize(double i, int range) {
                 for(int j = 0; j < lookup.length; j++) {
                     if(i < lookup[j]) return j;
                 }
@@ -112,6 +144,21 @@ public abstract class BiomeGrid {
             protected int getOctaves() {
                 return 5;
             }
+        }, CELLULAR {
+            @Override
+            protected int normalize(double i, int range) {
+                return NormalizationUtil.normalize(i, range);
+            }
+
+            @Override
+            protected FastNoise.NoiseType getNoiseType() {
+                return FastNoise.NoiseType.Cellular;
+            }
+
+            @Override
+            protected int getOctaves() {
+                return 5;
+            }
         };
 
         /**
@@ -120,8 +167,9 @@ public abstract class BiomeGrid {
          * @param i - The noise value to normalize.
          * @return int - The normalized value.
          */
-        protected abstract int normalize(double i);
+        protected abstract int normalize(double i, int range);
         protected abstract FastNoise.NoiseType getNoiseType();
         protected abstract int getOctaves();
     }
+
 }
