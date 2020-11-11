a5d4d1712e56b1d3ec9e74aa51671afd7b254539
dfsek <dfsek@protonmail.com>
2020-11-10 23:20:56 -0700

    Allow interpolation lookups outside chunk

--- src/main/java/org/polydev/gaea/math/ChunkInterpolator3.java
+++ src/main/java/org/polydev/gaea/math/ChunkInterpolator3.java
@@ -131,6 +131,10 @@ public class ChunkInterpolator3 implements ChunkInterpolator {
      */
     @Override
     public double getNoise(double x, double y, double z) {
-        return interpGrid[((int) x) / 4][((int) y) / 4][((int) z) / 4].trilerp((x % 4) / 4, (y % 4) / 4, (z % 4) / 4);
+        return interpGrid[reRange(((int) x) / 4, 3)][reRange(((int) y) / 4, 63)][reRange(((int) z) / 4, 3)].trilerp((x % 4) / 4, (y % 4) / 4, (z % 4) / 4);
+    }
+
+    private static int reRange(int value, int high) {
+        return Math.max(Math.min(value, high), 0);
     }
 }
