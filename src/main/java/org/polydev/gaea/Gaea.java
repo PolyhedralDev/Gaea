162fb1887c96838906d0f3c815d7dd1db11b21be
Bud Gidiere <sgidiere@gmail.com>
2020-11-21 23:39:07 -0600

    Yell Twice as Loud about paper

--- src/main/java/org/polydev/gaea/Gaea.java
+++ src/main/java/org/polydev/gaea/Gaea.java
@@ -4,6 +4,7 @@ import org.bstats.bukkit.Metrics;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.polydev.gaea.util.PaperUtil;
 
 import java.io.File;
 
@@ -23,6 +24,7 @@ public class Gaea extends JavaPlugin {
         reloadConfig();
         FileConfiguration configuration = getConfig();
         debug = configuration.getBoolean("debug", false);
+        PaperUtil.checkPaper(this);
     }
 
     public static File getGaeaFolder(World w) {
