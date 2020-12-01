744997ba290f76a864c869cfd0bc859b15e3acc3
dfsek <dfsek@protonmail.com>
2020-11-30 17:23:39 -0700

    Tree changes

--- src/main/java/org/polydev/gaea/tree/Tree.java
+++ src/main/java/org/polydev/gaea/tree/Tree.java
@@ -1,10 +1,14 @@
 package org.polydev.gaea.tree;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
+import java.util.Set;
 
 public interface Tree {
     boolean plant(Location l, Random r, JavaPlugin main);
+
+    Set<Material> getSpawnable();
 }
