282ba09fadb1956477ece120d02c42308b3cac37
dfsek <dfsek@protonmail.com>
2020-12-06 16:54:12 -0700

    lol prerelease versioning has been switched this whole time

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,9 +101,9 @@ class Version(val major: String, val minor: String, val revision: String, val pr
 
     override fun toString(): String {
         return if (preRelease)
-            "$major.$minor.$revision"
-        else //Only use git hash if it's a prerelease.
             "$major.$minor.$revision-BETA+${getGitHash()}"
+        else //Only use git hash if it's a prerelease.
+            "$major.$minor.$revision"
     }
 }
 
